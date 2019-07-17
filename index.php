8d874412a85506d2ce2c15afbbe66b03b404c411
Sergey Kazimirskiy <xDall.by@gmail.com>
2019-07-17 21:30:21 +0300

    Prepare for prod 2

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ require 'vendor/autoload.php';
 <html>
 <head>
     <link rel="stylesheet" href="services/wcAchivments/css/bootstrap.css">
-    <link rel="stylesheet" href="services/fightsStata/css/normilize.css">
+    <link rel="stylesheet" href="services/wcAchivments/css/normilize.css">
 </head>
 
 <body>
